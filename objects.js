e996b541f949a80dff83a353a31eb4ef297c9890
Kristopher Ross <thekdr@yahoo.com>
2018-02-01 18:52:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,12 +1,12 @@
 var playlist = {
-  Artist Name: "Song Title"
-}
+  Artist: "Song"
+};
 
 function updatePlaylist(obj, artist, song) {
-  return Object.assign(obj, {[artist]: song} )
+  return Object.assign(obj, {[artist]: song} );
 }
 
 function removeFromPlaylist(obj, artist) {
   delete obj.artist;
-  return obj
+  return obj;
 }
